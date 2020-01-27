4b0546270292de0699bfffcca650f3168ac8b9ef
Serhii Puchkovskyi <serhiipuchkovskii@gmail.com>
2020-01-27 13:37:26 +0200

    Manipulate Records with force:recordData.

---
+++ lightning_data_service/lightning_data_service_manipulate_records/README.md
@@ -0,0 +1,2 @@
+### Manipulate Records with force:recordData
+https://trailhead.salesforce.com/content/learn/modules/lightning_data_service/lightning_data_service_manipulate_records
---
+++ lightning_data_service/lightning_data_service_manipulate_records/accEditController.js
@@ -0,0 +1,16 @@
+({
+	handleSaveRecord : function(component, event, helper) {
+        component.find("recordLoader").saveRecord($A.getCallback(function(saveResult) {
+            // use the recordUpdated event handler to handle generic logic when record is changed
+            if (saveResult.state === "SUCCESS" || saveResult.state === "DRAFT") {
+                // handle component related logic in event handler
+            } else if (saveResult.state === "INCOMPLETE") {
+                console.log("User is offline, device doesn't support drafts.");
+            } else if (saveResult.state === "ERROR") {
+                console.log('Problem saving record, error: ' + JSON.stringify(saveResult.error));
+            } else {
+                console.log('Unknown problem, state: ' + saveResult.state + ', error: ' + JSON.stringify(saveResult.error));
+            }
+        }));
+	}
+})
